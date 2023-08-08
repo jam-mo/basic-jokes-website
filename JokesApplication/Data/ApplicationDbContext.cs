4646b6f16356a9ff9dcbb99c16ef676f5c3e41d2
Jamie <jamiemolseed@hotmail.co.uk>
2023-08-08 08:45:26 +0100

    Add project files.

---
+++ JokesApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesApplication.Models;
+
+namespace JokesApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesApplication.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ JokesApplication/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesApplication.Models
+{
+    public class Joke
+    {
+        public string Id { get; set; }
+        public string JokeQuestion  { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        { // ctor + tabx2 will auto create constructor
+            
+        }
+    }
+}
